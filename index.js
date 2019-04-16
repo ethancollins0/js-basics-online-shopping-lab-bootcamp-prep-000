cf8d7f19f785b9656773a220e38377d51517f74d
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 22:34:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,6 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  var number = parseInt(cardNumber)
   if (cardNumber === 'undefined') {
     return "Sorry, we don't have a credit card on file for you."
   } else {
