d96b08b1be652a9c8532dde258acc1f75e55dbbd
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 21:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,28 +16,31 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var cartCopy = cart.slice();
-  if (cartCopy.length > 0) {
-      var cartList = "In your cart, you have "
-    cartList += `${cart[0].itemName} at $${cart[0].itemPrice}`
-    cartCopy.shift()
-    while (cartCopy.length > 0) {
-      if (cartCopy.length > 1) {
-        cartList += `, ${cart[0].itemName} at $${cart[0].itemPrice}`
-        cartCopy.shift()
-      } else {
-          cartList += `, and ${cart[0].itemName} at $${cart[0].itemPrice}`
-          cartCopy.shift()
-      }
-
-    }
-    cartList += "."
-    return cartList
-  } else {
-    return "Your shopping cart is empty."
-  }
+  
 }
+/*
+var cartCopy = cart.slice();
+if (cartCopy.length > 0) {
+    var cartList = "In your cart, you have "
+  cartList += `${cart[0].itemName} at $${cart[0].itemPrice}`
+  cartCopy.shift()
+  while (cartCopy.length > 0) {
+    if (cartCopy.length > 1) {
+      cartList += `, ${cart[0].itemName} at $${cart[0].itemPrice}`
+      cartCopy.shift()
+    } else {
+        cartList += `, and ${cart[0].itemName} at $${cart[0].itemPrice}`
+        cartCopy.shift()
+    }
 
+  }
+  cartList += "."
+  return cartList
+} else {
+  return "Your shopping cart is empty."
+}
+}
+*/
 function total() {
   // write your code here
 }
