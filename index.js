e0a8fdd98ec0db2eb8f62e54aa310e879d8e0db5
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 22:24:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber < 1) {
+  if (parseInt(cardNumber) < 1) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = []
