e7e53afe5347fa330afbe7defe586dba8adf60ff
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 20:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   Object cart.item = {itemName: `${item},` itemPrice: (Math.floor(Math.random()*100)+1)}
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
