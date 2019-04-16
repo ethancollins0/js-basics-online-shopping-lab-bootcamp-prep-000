566e717160c740d71bca4f38ee7acc138eeaeefa
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 22:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ function total() {
 function removeFromCart(item) {
   var cartCopy = cart.slice()
   for(let i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) {
-      cart.s
+    if (cart[i].itemName === `${item}`) {
+      delete cart[`${item}`]
       return cart
     }
   }
