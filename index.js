cbd26e3d069e37d1569356a3685114ed380aaabe
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 22:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ function placeOrder(cardNumber) {
   } else {
     cart = []
     var total = total()
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
