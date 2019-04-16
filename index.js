1b52a5e687b50ab241c07ffbfc705b743390612e
Ethan Collins <ethantcollins98@gmail.com>
2019-04-16 22:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function placeOrder(cardNumber) {
   if (cardNumber.length < 1) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
+    cart = []
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
